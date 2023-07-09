a0d804da96b231608a9c6b0757887d196b2296c0
qodirovdoston806 <qodirovdoston806@gmail.com>
2023-07-09 23:06:40 +0500

    create React

--- demo/src/Componts/Navbar.jsx
+++ demo/src/Componts/Navbar.jsx
@@ -1,6 +1,8 @@
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="container mx-auto py-[10px] sm:px-[30px] px-[10px]">
+      <div className="text-red-700">Navbar</div>
+    </div>
   )
 }
