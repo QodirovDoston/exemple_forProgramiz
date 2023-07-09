a5a436733d89fd9c7cf288616e484f8d13cbf595
KingA2009 <ajamoliddinov12@gmail.com>
2023-07-09 23:24:15 +0500

    fix

--- demo/src/Componts/Navbar.jsx
+++ demo/src/Componts/Navbar.jsx
@@ -4,6 +4,7 @@ export default function Navbar() {
     <div className="container mx-auto py-[10px] sm:px-[30px] px-[10px]">
       <div className="text-red-700">Navbar</div>
       <h1 className="text-red-700 hover:text-light-50">Hello Navbar</h1>
+      <h1>Asad sila eplab bo`lmayati uji</h1>
     </div>
   )
 }
