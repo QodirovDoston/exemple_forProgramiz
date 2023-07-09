19a473c902516e246a090f8a50f41eee44fe3658
qodirovdoston806 <qodirovdoston806@gmail.com>
2023-07-09 23:09:50 +0500

    ad navbar

--- demo/src/Componts/Navbar.jsx
+++ demo/src/Componts/Navbar.jsx
@@ -3,6 +3,7 @@ export default function Navbar() {
   return (
     <div className="container mx-auto py-[10px] sm:px-[30px] px-[10px]">
       <div className="text-red-700">Navbar</div>
+      <h1 className="text-red-700 hover:text-light-50">Hello Navbar</h1>
     </div>
   )
 }
